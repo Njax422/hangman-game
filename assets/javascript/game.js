5ac27e7a6c566d3aea96c3803acda05a8b106666
Bootcamp <njackson422@gmail.com>
2017-09-11 20:35:52 -0400

    update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,47 +2,76 @@ var words = ['grumpycat', 'lolcat', 'success kid', 'kermit'];
 var word = words[Math.floor(Math.random()*words.length)];
 	console.log('word: ', word);
 var answer = [];
-	for (var i=0; i<word.length; i++){
-		answer.push("-");
-	}
+
 var remainingLetters=word.length;
 var guess; 
 var guesses = [];
 var guessesRemaining = 6;
 var winPoints=0;
 
+	for (var i=0; i<word.length; i++){
+		answer.push("-");
+	}
 
-document.getElementById('currentWord').innerHTML= answer.join("");
-document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
-document.getElementById("winPoints").innerHTML=winPoints; 
+	document.getElementById('currentWord').innerHTML= answer.join("");
+	document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
+	document.getElementById("winPoints").innerHTML=winPoints; 
 
-document.onkeyup = function(event){
-	console.log(event);
+	document.onkeyup = function(event){
+		console.log(event);
 
 
-	if (word.indexOf(event.key) > -1) {
-	
- 		for (var i = 0; i < word.length; i++) {
- 			if (event.key === word[i]) {
- 				answer[i] = event.key;	
- 			}
- 		}
- 		if (answer.indexOf("-")===-1) {
- 			winPoints++;
- 			document.getElementById("winPoints").innerHTML=winPoints;
- 		}
+		if (word.indexOf(event.key) > -1) {
+		
+	 		for (var i = 0; i < word.length; i++) {
+	 			if (event.key === word[i]) {
+	 				answer[i] = event.key;	
+	 			}
+	 		}
+	 		if (answer.indexOf("-")===-1) {
+	 			winPoints++;
+	 			document.getElementById("winPoints").innerHTML=winPoints;
+	 		}
 
- 		document.getElementById('currentWord').innerHTML= answer.join("");	
- 	} else {
- 		
- 		if(guesses.indexOf(event.key)===-1) {
-	 		guessesRemaining--;
-	 		console.log(guessesRemaining);
-	 		document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
-	 		guesses.push(event.key);
-	 		document.getElementById("lettersGuessed").innerHTML=guesses;
+	 		document.getElementById('currentWord').innerHTML= answer.join("");	
+	 	} else {
+	 		
+	 		if(guesses.indexOf(event.key)===-1) {
+		 		guessesRemaining--;
+		 		console.log(guessesRemaining);
+		 		document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
+		 		guesses.push(event.key);
+		 		document.getElementById("lettersGuessed").innerHTML=guesses;
+		 	}
+
+
+
+
+
+
+
+
+
+
+
+
+		 	
 	 	}
- 	}
 
 
- };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 };
