db6638d6940b922fc4f21b2849d2ea83e2d28aeb
Bootcamp <njackson422@gmail.com>
2017-09-13 13:30:31 -0400

    update files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
-var words = ['grumpycat',
+var words = [
+	{word:'grumpy cat',
+	image: ""},
 	'lolcat',
-	'successkid',
+	'success kid',
 	'kermit',
-	'willywonka',
+	'willy wonka',
 	'sheldon',
 	'oprah',
 	'philosoraptor'];
-var word = words[Math.floor(Math.random()*words.length)];
+	console.log(words[0].word);
+var word = words[0].word[Math.floor(Math.random()*words.length)];
 	console.log('word: ', word);
 var answer = [];
 var guess; 
@@ -16,7 +19,11 @@ var winPoints=0;
 var keepPlaying;
 
 	for (var i=0; i<word.length; i++){
+		if (word[i]===" ") {
+			answer.push(" ");
+		} else {
 		answer.push("-");
+		}
 	}
 
 	document.getElementById('currentWord').innerHTML= answer.join("");
@@ -26,41 +33,45 @@ var keepPlaying;
 	document.onkeyup = function(event){
 		console.log(event);
 
+		if (!/^[A-Za-z]/i.test(event.key)) {
+	        alert("Invalid characters");
+	    } else {
 
-		if (word.indexOf(event.key) > -1) {
-		
-	 		for (var i = 0; i < word.length; i++) {
-	 			if (event.key === word[i]) {
-	 				answer[i] = event.key;	
-	 			}
-	 		}
-	 		document.getElementById('currentWord').innerHTML= answer.join("");
+			if (word.indexOf(event.key) > -1) {
+			
+		 		for (var i = 0; i < word.length; i++) {
+		 			if (event.key === word[i]) {
+		 				answer[i] = event.key;	
+		 			}
+		 		}
+		 		document.getElementById('currentWord').innerHTML= answer.join("");
 
-	 		if (answer.indexOf("-")===-1) {
-	 			winPoints++;
-	 			document.getElementById("winPoints").innerHTML=winPoints;
-		 			var keepPlaying = confirm("You won! Want to play again?");
-			 			if (keepPlaying) {
-			 				reset();
-				 		}
-	 		}
+		 		if (answer.indexOf("-")===-1) {
+		 			winPoints++;
+		 			document.getElementById("winPoints").innerHTML=winPoints;
+			 			var keepPlaying = confirm("You won! Want to play again?");
+				 			if (keepPlaying) {
+				 				reset();
+					 		}
+		 		}
 
-	 	} else {
-	 		
-	 		if(guesses.indexOf(event.key)===-1) {
-		 		guessesRemaining--;
-		 		console.log(guessesRemaining);
-		 		document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
-		 		guesses.push(event.key);
-		 		document.getElementById("lettersGuessed").innerHTML=guesses;
-				if (guessesRemaining===0) {
-		 			keepPlaying = confirm("You lose! Want to play again?");
-			 			if (keepPlaying) {
-			 				reset();
-				 		}
-				}
+		 	} else {
+		 		
+		 		if(guesses.indexOf(event.key)===-1) {
+			 		guessesRemaining--;
+			 		console.log(guessesRemaining);
+			 		document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
+			 		guesses.push(event.key);
+			 		document.getElementById("lettersGuessed").innerHTML=guesses;
+					if (guessesRemaining===0) {
+			 			keepPlaying = confirm("You lose! Want to play again?");
+				 			if (keepPlaying) {
+				 				reset();
+					 		}
+					}
+			 	}
 		 	}
-	 	}
+		 }
 	 };
 	 function reset() {
 		guessesRemaining = 6;
