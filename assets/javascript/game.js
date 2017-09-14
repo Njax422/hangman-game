0c8f889750fd6b6a9e58a7c76a7c0e69892de3e7
Bootcamp <njackson422@gmail.com>
2017-09-13 22:16:53 -0400

    adds all working logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,40 @@
 var words = [
-	{word:'grumpy cat',
-	image: ""},
-	'lolcat',
-	'success kid',
-	'kermit',
-	'willy wonka',
-	'sheldon',
-	'oprah',
-	'philosoraptor'];
-	console.log(words[0].word);
-var word = words[0].word[Math.floor(Math.random()*words.length)];
+	{
+		word:'grumpy cat',
+		image: ""
+	},
+	{
+		word:'lolcat',
+		image: ""
+	},
+	{
+		word:'success kid',
+		image: ""
+	},
+	{
+		word:'kermit',
+		image: ""
+	},
+	{
+		word:'willy wonka',
+		image: ""
+	},
+	{
+		word:'sheldon',
+		image: ""
+	},
+	{
+		word:'oprah',
+		image: ""
+	},
+	{
+		word:'philosoraptor',
+		image: ""
+	}];
+var index = Math.floor(Math.random()*words.length);
+var word = words[index].word;
 	console.log('word: ', word);
 var answer = [];
-var guess; 
 var guesses = [];
 var guessesRemaining = 6;
 var winPoints=0;
@@ -22,39 +44,40 @@ var keepPlaying;
 		if (word[i]===" ") {
 			answer.push(" ");
 		} else {
-		answer.push("-");
+			answer.push("-");
 		}
 	}
 
-	document.getElementById('currentWord').innerHTML= answer.join("");
-	document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
-	document.getElementById("winPoints").innerHTML=winPoints; 
+document.getElementById('currentWord').innerHTML= answer.join("");
+document.getElementById('guessesRemaining').innerHTML=guessesRemaining;
+document.getElementById('winPoints').innerHTML=winPoints; 
 
 	document.onkeyup = function(event){
 		console.log(event);
 
-		if (!/^[A-Za-z]/i.test(event.key)) {
+		if (!/^[A-Za-z]$/i.test(event.key)) {
 	        alert("Invalid characters");
 	    } else {
 
 			if (word.indexOf(event.key) > -1) {
-			
 		 		for (var i = 0; i < word.length; i++) {
 		 			if (event.key === word[i]) {
 		 				answer[i] = event.key;	
 		 			}
 		 		}
+
 		 		document.getElementById('currentWord').innerHTML= answer.join("");
 
 		 		if (answer.indexOf("-")===-1) {
 		 			winPoints++;
 		 			document.getElementById("winPoints").innerHTML=winPoints;
+		 			setTimeout(function() {
 			 			var keepPlaying = confirm("You won! Want to play again?");
 				 			if (keepPlaying) {
 				 				reset();
-					 		}
-		 		}
-
+							}
+					}, 100);
+				}
 		 	} else {
 		 		
 		 		if(guesses.indexOf(event.key)===-1) {
@@ -73,16 +96,23 @@ var keepPlaying;
 		 	}
 		 }
 	 };
-	 function reset() {
+
+	function reset() {
+	 	guesses = [];
 		guessesRemaining = 6;
-		document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
-		guesses = [];
-		document.getElementById("lettersGuessed").innerHTML=guesses;
 		answer.length = 0;
-		word = words[Math.floor(Math.random()*words.length)];
-			for (var i=0; i<word.length; i++){
-				answer.push("-");
-			}
-		document.getElementById('currentWord').innerHTML= answer.join("");
+		index = Math.floor(Math.random()*words.length);	
+		word = words[index].word;
 		console.log(word);
+			for (var i=0; i<word.length; i++){
+				if (word[i]===" ") {
+					answer.push(" ");
+				} else {
+					answer.push("-");
+				}
+			}
+		document.getElementById("lettersGuessed").innerHTML=guesses;
+		document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
+		document.getElementById('currentWord').innerHTML= answer.join("");
 	}
+	
